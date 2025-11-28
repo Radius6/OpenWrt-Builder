0479c10c225dfd0b643e5369639209d0306fc2fe
Radius6 <dely2016@gmail.com>
2025-11-28 21:45:16 +0800

    Update diy-part1-x86-64.sh

--- immortalwrt/diy-part1-x86-64.sh
+++ immortalwrt/diy-part1-x86-64.sh
@@ -15,12 +15,6 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# 使用 O2 级别的优化
-sed -i 's,Os,O2 -march=x86-64-v2,g' include/target.mk
 
-# 关闭 Spectre & Meltdown 补丁
-sed -i 's,noinitrd,noinitrd mitigations=off,g' target/linux/x86/image/grub-efi.cfg
-sed -i 's,noinitrd,noinitrd mitigations=off,g' target/linux/x86/image/grub-iso.cfg
-sed -i 's,noinitrd,noinitrd mitigations=off,g' target/linux/x86/image/grub-pc.cfg
